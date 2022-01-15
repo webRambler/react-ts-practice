d6cd5ba47a9024f4bcb7181169fde2021a215718
webRambler <talent1203@163.com>
2022-01-15 16:07:59 +0800

    feat：引入react-router-dom路由配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
+import {Link, Outlet, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
   return (
     <div className="App">
       Hello world!
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <div>
+        <Link to="/home">Home Page</Link> | 
+        <Link to="/about">About Page</Link>
+      </div>
+      <Outlet />
     </div>
   );
 }
---
+++ src/pages/About.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div>这是About页面</div>
+    )
+}
+
+export default About;
