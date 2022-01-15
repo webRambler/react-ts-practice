a3db2e8848c08cbc8d53098a103c052c707ff62e
webRambler <talent1203@163.com>
2022-01-15 18:18:25 +0800

    feat：修改reducer

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,13 +1,73 @@
+// import React, { useReducer } from 'react'
+
+// type StateType = {
+//     count: number;
+// }
+
+// type ActionType = {
+//     type: string;
+//     payload: number;
+// }
+
+// const initialState = {count: 0}
+
+// function countReducer(state: StateType, action: ActionType) {
+//     switch (action.type) {
+//         case 'increment':
+//             return {count: state.count + action.payload}
+//             break;
+//         case 'decrement':
+//             return {count: state.count - action.payload}
+//             break;
+//         default:
+//             return state
+//             break;
+//     }
+// }
+
+// export default function Counter() {
+//     const [state, dispatch] = useReducer(countReducer, initialState)
+
+//     const _increment = () => {
+//         dispatch({
+//             type: 'increment',
+//             payload: 1
+//         })
+//     }
+    
+//     const _decrement = () => {
+//         dispatch({
+//             type: 'decrement',
+//             payload: 1
+//         })
+//     }
+
+//     return (
+//         <div>
+//             <p>Your count is {state.count}</p>
+//             <button onClick={_increment}>increment</button>&nbsp;&nbsp;
+//             <button onClick={_decrement}>decrement</button>
+//         </div>
+//     )
+// }
+
+
 import React, { useReducer } from 'react'
 
 type StateType = {
     count: number;
 }
 
-type ActionType = {
-    type: string;
+type UpdateActionType = {
+    type: 'increment' | 'decrement';
     payload: number;
 }
+
+type ResetActionType = {
+    type: 'reset';
+}
+
+type ActionType = UpdateActionType | ResetActionType;
 
 const initialState = {count: 0}
 
@@ -19,8 +79,8 @@ function countReducer(state: StateType, action: ActionType) {
         case 'decrement':
             return {count: state.count - action.payload}
             break;
-        default:
-            return state
+        case 'reset':
+            return initialState
             break;
     }
 }
@@ -41,12 +101,19 @@ export default function Counter() {
             payload: 1
         })
     }
+    
+    const _reset = () => {
+        dispatch({
+            type: 'reset'
+        })
+    }
 
     return (
         <div>
             <p>Your count is {state.count}</p>
             <button onClick={_increment}>increment</button>&nbsp;&nbsp;
-            <button onClick={_decrement}>decrement</button>
+            <button onClick={_decrement}>decrement</button>&nbsp;&nbsp;
+            <button onClick={_reset}>reset</button>
         </div>
     )
 }
